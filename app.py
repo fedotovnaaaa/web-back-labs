d013d178a24531fa86ab2665afddc4049247eb99
Julia Fedotova <julia.fedotoff.2005@gmail.com>
2025-09-13 23:14:28 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, redirect
 import datetime
 app = Flask(__name__)
 
@@ -65,3 +65,7 @@ def counter():
 </html>
 '''
 
+
+@app.route("/info")
+def info():
+    return redirect("/author")
