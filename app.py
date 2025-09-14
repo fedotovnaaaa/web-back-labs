2028c69e548c7dac501ab17c2d7b16ec4a2b59ab
Julia Fedotova <julia.fedotoff.2005@gmail.com>
2025-09-14 17:53:41 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ app = Flask(__name__)
 def not_found(err):
     return "нет такой страницы", 404
 
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return '''<!doctype html>
         <html>
            <body>
                <h1>web-сервер на flask</h1>
-               <a href="/author">author</a>
+               <a href="/lab1/author">author</a>
            </body>
         </html>''', 200, {
             'X-Server': 'sample',
             'Content-Type': 'text/plain; charset=utf-8'
         }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Федотова Юлия Сергеевна"
     group = "ФБИ-31"
@@ -31,7 +31,7 @@ def author():
                <p>Студент: """ + name + """</p>
                <p>Группа: """ + group + """</p>
                <p>Факультет: """ + faculty + """</p>
-               <a href="/web">web</a>
+               <a href="/lab1/web">web</a>
            </body>
         </html>"""
 
@@ -39,6 +39,7 @@ def author():
 def image():
     path = url_for("static", filename="oak.jpg")
     style = url_for("static", filename="lab1.css")
+    
     return '''
 <!doctype html>
 <html>
@@ -55,7 +56,7 @@ def image():
 
 count = 0
 
-@app.route('/counter')
+@app.route('/lab1/counter')
 def counter():
     global count
     count +=1
@@ -76,7 +77,7 @@ def counter():
 </html>
 '''
 
-@app.route('/counter/clean')
+@app.route('/lab1/counter/clean')
 def clean_counter():
     global count
     count = 0
@@ -92,9 +93,9 @@ def clean_counter():
 </html>
 '''
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 
 
 @app.route("/lab1/created")
