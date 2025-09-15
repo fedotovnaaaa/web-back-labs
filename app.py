e49661bffd56653ebac730596496b821a99d859b
Julia Fedotova <julia.fedotoff.2005@gmail.com>
2025-09-15 20:54:25 +0700

    Коды ответов HTTP

--- app.py
+++ app.py
@@ -4,7 +4,149 @@ app = Flask(__name__)
 
 @app.errorhandler(404)
 def not_found(err):
-    return "нет такой страницы", 404
+    return "тю-тю... нет такой страницы...", 404
+
+@app.errorhandler(400)
+def handle_400(error):
+    return bad_request()
+
+@app.errorhandler(401)
+def handle_401(error):
+    return unauthorized()
+
+@app.errorhandler(403)
+def handle_403(error):
+    return forbidden()
+
+@app.errorhandler(405)
+def handle_405(error):
+    return method_not_allowed()
+
+@app.errorhandler(418)
+def handle_418(error):
+    return teapot()
+
+@app.route('/400')
+def bad_request():
+    style = url_for("static", filename="lab1.css")
+
+    return '''
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="''' + style + '''">
+    <title>400 Bad Request</title>
+</head>
+<body class="err">
+    <h1 class="err">400 Bad Request</h1>
+    <p class="err">Сервер не может обработать запрос из-за некорректного синтаксиса.</p>
+    <p class="err">Пожалуйста, проверьте правильность вашего запроса.</p>
+</body>
+</html>
+''', 400
+
+@app.route('/401')
+def unauthorized():
+    style = url_for("static", filename="lab1.css")
+
+    return '''
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="''' + style + '''">
+    <title>401 Unauthorized</title>
+</head>
+<body class="err">
+    <h1 class="err">401 Unauthorized</h1>
+    <p class="err">Требуется аутентификация для доступа к данному ресурсу.</p>
+    <p class="err">Пожалуйста, предоставьте корректные учетные данные.</p>
+</body>
+</html>
+''', 401
+
+@app.route('/402')
+def payment_required():
+    style = url_for("static", filename="lab1.css")
+
+    return '''
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="''' + style + '''">
+    <title>402 Payment Required</title>
+</head>
+<body class="err">
+    <h1 class="err">402 Payment Required</h1>
+    <p class="err">Для доступа к данному ресурсу требуется оплата.</p>
+    <p class="err">Этот код зарезервирован для будущего использования.</p>
+</body>
+</html>
+''', 402
+
+@app.route('/403')
+def forbidden():
+    style = url_for("static", filename="lab1.css")
+
+    return '''
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="''' + style + '''">
+    <title>403 Forbidden</title>
+</head>
+<body class="err">
+    <h1 class="err">403 Forbidden</h1>
+    <p class="err">Доступ к запрошенному ресурсу запрещен.</p>
+    <p class="err">У вас нет необходимых прав для просмотра этой страницы.</p>
+</body>
+</html>
+''', 403
+
+@app.route('/405')
+def method_not_allowed():
+    style = url_for("static", filename="lab1.css")
+
+    return '''
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="''' + style + '''">
+    <title>405 Method Not Allowed</title>
+</head>
+<body class="err">
+    <h1 class="err">405 Method Not Allowed</h1>
+    <p class="err">Метод запроса не поддерживается для данного ресурса.</p>
+    <p class="err">Пожалуйста, используйте допустимый HTTP-метод.</p>
+</body>
+</html>
+''', 405
+
+@app.route('/418')
+def teapot():
+    style = url_for("static", filename="lab1.css")
+
+    return '''
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="''' + style + '''">
+    <title>418 I'm a teapot</title>
+</head>
+<body class="err">
+    <h1 class="err">418 I'm a teapot</h1>
+    <p class="err">Я - чайник и не могу заваривать кофе.</p>
+    <p class="err">Это шуточный код ошибки из RFC 2324 (Hyper Text Coffee Pot Control Protocol).</p>
+</body>
+</html>
+''', 418
+
+
 
 @app.route("/")
 @app.route("/index")
