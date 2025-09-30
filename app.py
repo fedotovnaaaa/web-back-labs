23816840c56b89b27b9faebe809b7503cf3d58d5
Julia Fedotova <julia.fedotoff.2005@gmail.com>
2025-09-30 14:39:24 +0700

    добавление цветка

--- app.py
+++ app.py
@@ -492,7 +492,7 @@ def a():
 def a2():
     return 'со слэша'
 
-flower_list = ('роза', 'тюльпан', 'пион', 'гипсофила')
+flower_list = ['роза', 'тюльпан', 'пион', 'гипсофила', 'ромашка']
 
 @app.route('/lab2/flowers/<int:flower_id>')
 def flowers(flower_id):
@@ -500,3 +500,18 @@ def flowers(flower_id):
         abort(404)
     else:
         return "Цветок: " + flower_list[flower_id]
+
+@app.route('/lab2/add_flower/<name>')
+def add_flower(name):
+    flower_list.append(name)
+    return f'''
+<!doctype html>
+<html>
+    <body>
+        <h1>Добавлен новый цветок</h1>
+        <p>Добавлен цветок: {name} </p>
+        <p>Всего цветов: {len(flower_list)} </p>
+        <p>Полный список: {flower_list} </p>
+    </body>
+</html>
+'''
