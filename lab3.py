052c51cc399f991f8b631d100ffcce7e9d4cda0d
Julia Fedotova <julia.fedotoff.2005@gmail.com>
2025-10-14 15:49:44 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -46,3 +46,48 @@ def form1():
                          user=user, age=age, sex=sex, 
                          errors=errors)  # Передаем один словарь с ошибками
 
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('lab3/order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+    
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template('lab3/pay.html', price=price)
+
+
+@lab3.route('/lab3/success')
+def success():
+    price = 0
+    drink = request.args.get('drink')
+
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+    
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template('lab3/success.html', price=price)
+
