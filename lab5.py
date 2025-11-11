087c1b08e09904bbb6f2501bbe9b47fa88582d3f
Julia Fedotova <julia.fedotoff.2005@gmail.com>
2025-11-11 20:48:06 +0700

    Создание статьи

--- lab5.py
+++ lab5.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, redirect
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -8,6 +8,7 @@ lab5 = Blueprint('lab5', __name__)
 @lab5.route('/lab5/')
 def lab():
     return render_template('lab5/lab5.html', login=session.get('login'))
+
 
 def db_connect():
     conn = psycopg2.connect(
@@ -82,4 +83,28 @@ def login():
     session['login'] = login
     db_close(conn, cur)
     return render_template('lab5/success_login.html', login=login)
+
+
+@lab5.route('/lab5/create', methods = ['GET', 'POST'])
+def create():
+    login=session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+    
+    if request.method == 'GET':
+        return render_template('lab5/create_article.html')
+    
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+
+    conn, cur = db_connect()
+
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
+    user_id = cur.fetchone()["id"]
+
+    cur.execute(f"INSERT INTO articles(user_id, title, article_text) \
+                VALUES ({user_id}, '{title}', '{article_text}');")
+    
+    db_close(conn, cur)
+    return redirect('/lab5')
 
