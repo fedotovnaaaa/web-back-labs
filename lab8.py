4666cc7be6c90b05d2fe36e22b67de83e06527d0
Julia Fedotova <julia.fedotoff.2005@gmail.com>
2025-12-10 20:28:25 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -3,6 +3,7 @@ from os import path
 from db import db
 from db.models import users, articles
 from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, login_required, current_user
 
 
 lab8 = Blueprint('lab8', __name__)
@@ -43,4 +44,30 @@ def register():
     return redirect('/lab8/')
 
 
-   
+@lab8.route('/lab8/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    # Валидация обязательных полей
+    if not login_form or not password_form:
+        return render_template('lab8/login.html', 
+                               error="Заполните поля")
+
+    user = users.query.filter_by(login = login_form).first()
+
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember = False)
+            return redirect('/lab8/')
+        
+    return render_template('/lab8/login.html', 
+                           error = 'Ошибка входа: логин и/или пароль неверны')
+
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return "Список статей"
